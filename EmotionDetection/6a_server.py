5f87de6784fe8921ec73edc1a0c3b9f4a8b1e317
THONG <thonghv.pine@gmail.com>
2026-04-01 21:45:14 +0700

    Add Flask server for web deployment

---
+++ EmotionDetection/6a_server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is <b>{}</b>.".format(
+        response['anger'],
+        response['disgust'],
+        response['fear'],
+        response['joy'],
+        response['sadness'],
+        response['dominant_emotion']
+    )
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
